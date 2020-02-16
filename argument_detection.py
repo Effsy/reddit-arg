204d13b97ec7390f59786439955350d86b46e2fa
Effsy <jardinomidvaran@hotmail.co.uk>
2020-02-16 01:06:09 +0000

    Add argument detection class

---
+++ argument_detection.py
@@ -0,0 +1,86 @@
+import joblib
+
+import nltk
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+import string
+
+import numpy as np
+
+class ArgumentPredictor():
+
+    def __init__(self):
+        nltk.download('wordnet')
+
+        self.ngram_model = joblib.load('./models/arg_detection_ngram_model.pkl')
+        self.meta_model = joblib.load('./models/arg_detection_meta_model.pkl')
+
+    # Lemmatize all words in a sentence. Uses PoS to identify lemma
+    def lemmatize_sentence(self, sentence):
+        wnl = WordNetLemmatizer()
+        lemma_array = []
+
+        for word, tag in pos_tag(word_tokenize(sentence)):
+            if tag.startswith("NN"):
+                lemma_array.append(wnl.lemmatize(word, pos='n'))
+            elif tag.startswith('VB'):
+                lemma_array.append(wnl.lemmatize(word, pos='v'))
+            elif tag.startswith('JJ'):
+                lemma_array.append(wnl.lemmatize(word, pos='a'))
+            else:
+                lemma_array.append(word)
+
+        return ' '.join(lemma_array)
+
+    # Extract features from a sentence
+    def extract_features(self, sentence):
+        # Predictions based on ngrams from classifier trained on all data       
+
+        # Lemmatize
+        sentence_lemma = [self.lemmatize_sentence(sentence)]
+        
+        ngram_prediction = 1 if self.ngram_model.predict(sentence_lemma)[0] == "arg" else 0
+        
+        # Sentiment of sentence
+        sid = SentimentIntensityAnalyzer()
+        sentiment = sid.polarity_scores(sentence)['compound']
+        
+        # Sentence Length
+        sentence_length = len(word_tokenize(sentence))
+        
+        # Number of each parts of speech
+        pos_counts = []
+        nn = 0
+        vb = 0
+        jj = 0
+        for word, pos in pos_tag(word_tokenize(sentence)):
+            if pos.startswith('NN'):
+                nn += 1
+            elif pos.startswith('VB'):
+                vb += 1
+            elif pos.startswith('JJ'):
+                jj += 1
+        pos_counts.append([nn, vb, jj])
+        
+        # Combine features into numpy matrix
+        return np.column_stack([ngram_prediction, sentiment, sentence_length, pos_counts])
+
+    def predict_argument(self, sentence):
+        # Remove punctuation
+        sentence_cleaned = sentence.translate(str.maketrans('', '', string.punctuation))
+        # print(sentence_cleaned)
+        features = self.extract_features(sentence_cleaned)
+        # print(features)
+        return self.meta_model.predict(features)
+
+
+    def filter_arguments(self, sentence_list):
+        """Filters all non-argumentative sentences from the list and returns only those that are argumentative.
+        Uses predictive models, which may lead to some incorrect classifications"""
+
+        
+
+
